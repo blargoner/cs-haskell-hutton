38f7ec2bd08a7fa93bae23baf0fd306160dd57d9
John Peloquin <github@blargon.net>
2014-03-03 03:25:14 -0500

    sets

---
+++ sets.hs
@@ -0,0 +1,35 @@
+-- lists
+sub :: Eq a => [a] -> [a] -> Bool
+sub xs ys = and [ elem x ys | x <- xs ]
+
+-- set
+data Set = Set [Set] deriving Show
+
+instance Eq Set where
+    Set xs == Set ys = sub xs ys && sub ys xs
+
+-- singleton
+single :: Set -> Set
+single x = Set [x]
+
+-- pair
+pair :: Set -> Set -> Set
+pair x y = Set [x,y]
+
+-- union
+union :: Set -> Set
+union (Set xs) = Set (concat [ ys | Set ys <- xs ])
+
+union2 :: Set -> Set -> Set
+union2 x y = union (pair x y)
+
+-- successor
+successor :: Set -> Set
+successor x = union2 x (single x)
+
+-- ordinal
+ordinal :: Int -> Set
+ordinal 0 = Set []
+ordinal n | n > 0 = successor (ordinal (n - 1))
+
+omega = Set (map ordinal [1..])
